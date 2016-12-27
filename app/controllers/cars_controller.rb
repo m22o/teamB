9d6daa8f793565d661f1785b6080d7b90e281032
kurouw <github0220@gmail.com>
2016-12-28 00:14:52 +0900

    β版Car ができたよ

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,49 @@
 class CarsController < ApplicationController
-  def index
+  before_action :set_car, only: [:show, :edit, :update]
+
+  def new
+    @car = Car.new
   end
+
+  def show
+  end
+
+  def create
+    @car = Car.create(car_params)
+    respond_to do |format|
+      if @car.save
+        format.html { redirect_to @car, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @car}
+      else
+        format.html { render :new }
+        format.json { render json: @car.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    set_car
+  end
+
+  def update
+    respond_to do |format|
+      if @car.update(user_params)
+        format.html { redirect_to @car, notice: 'koushin dekitayo'}
+        format.json { head :no_conten}
+      else
+        format.html { render action: 'edit'}
+        format.json { render json: @car.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def car_params
+    params.require(:car).permit(:car_number,:color)
+  end
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :users, expect:[:create,:update]
+  resources :cars, expect:[:create,:update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
